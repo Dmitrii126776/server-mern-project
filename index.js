c1216a979225f513406f4f4f3625b3b0ccdf5260
Dmitrii126776 <dukek67@gmail.com>
2023-03-13 23:52:01 -0400

    change index directory

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from "cors";
-import errorHandler from "./modules/core/errorHandler";
-import dbConnect from "./modules/core/db";
-import logger from "./modules/core/logger";
-import parseResponse from "./modules/core/parseResponse";
-import routes from "./modules/core/routes";
+import errorHandler from "./src/modules/core/errorHandler";
+import dbConnect from "./src/modules/core/db";
+import logger from "./src/modules/core/logger";
+import parseResponse from "./src/modules/core/parseResponse";
+import routes from "./src/modules/core/routes";
 import dotenv from "dotenv";
 
 const app = express();
