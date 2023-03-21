a4b217b8154a78001c52f47e39dca92d2e209b9d
Dmitrii126776 <dukek67@gmail.com>
2023-03-21 19:00:34 -0400

    update cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ logger(app)
 parseResponse(app)
 app.use(cors({
     credentials: true,
-    origin: 'https://client-mern-auth.netlify.app'
+    origin: 'https://client-mern-auth.netlify.app' || 'http://localhost:3000'
 }))
 
 routes(app)
