7b236e5ae42067574754015f1754fa7d1b1395be
Dmitrii126776 <dukek67@gmail.com>
2023-04-11 19:27:38 -0400

    NumbersRouter.js

--- src/modules/number/NumbersRouter.js
+++ src/modules/number/NumbersRouter.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import postNumber from "./postNumber";
 import getNumber from "./getNumber";
-import updateNumber from "./updateNumber";
+// import updateNumber from "./updateNumber";
 import updateNumberById from "./updateNumberById";
 
 const numbersRouter = Router();
@@ -9,7 +9,7 @@ const numbersRouter = Router();
 
 numbersRouter.post('/', postNumber)
 numbersRouter.get('/', getNumber)
-numbersRouter.patch('/', updateNumber)
+// numbersRouter.patch('/', updateNumber)
 numbersRouter.patch('/:numberId', updateNumberById)
 
 export default numbersRouter;
