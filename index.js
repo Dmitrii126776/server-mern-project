b24e7e6debf2ea80527b163f38144d75739de8cd
Dmitrii126776 <dukek67@gmail.com>
2023-06-14 19:51:12 -0400

    origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ parseResponse(app)
 //cors(app)
 app.use(cors({
     credentials: true,
-    origin: 'https://client-mern-auth.netlify.app' || 'http://localhost:3000'
+    origin: 'https://client-mern-auth.netlify.app'
 }))
 
 routes(app)
