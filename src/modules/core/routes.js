fc94c353753d6a3ccbfb504d167c22a006bcbe79
Dmitrii126776 <dukek67@gmail.com>
2023-06-14 20:24:33 -0400

    HomeRouter.js

--- src/modules/core/routes.js
+++ src/modules/core/routes.js
@@ -4,9 +4,11 @@ import tasksRouter from "../tasks/TasksRouter";
 import cardsRouter from "../cards/CardsRouter";
 import animalsRouter from "../animals/AnimalsRouter";
 import numbersRouter from "../number/NumbersRouter";
+import homeRouter from "../home/HomeRouter";
 
 
 export default function routes(app) {
+    app.use('/', homeRouter)
     app.use('/', userRouter)
     app.use('/statuses', statusesRouter)
     app.use('/tasks', tasksRouter)
