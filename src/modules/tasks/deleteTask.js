68ed0b48be5d2b98e41a585a369e60bad175e54d
Dmitrii126776 <dukek67@gmail.com>
2024-04-01 22:24:18 -0400

    deleteTask.js

--- src/modules/tasks/deleteTask.js
+++ src/modules/tasks/deleteTask.js
@@ -6,9 +6,28 @@ export default function deleteTask(req, res) {
     Task.deleteOne({_id: taskId})
         .exec()
         .then(result => {
-            // res.status(202).json('Task was deleted');
-            res.status(202).json(result);
-        }).catch(err => {
-        res.status(402).send('Task was not deleted');
-    });
+            if (result.deletedCount === 1) {
+                res.status(200).json({ message: "Task deleted successfully", deletedTaskId: taskId });
+            } else {
+                res.status(404).json({ message: "Task not found" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message });
+        });
 }
+
+// import Task from './Model';
+//
+// export default function deleteTask(req, res) {
+//     const taskId = req.params.taskId;
+//
+//     Task.deleteOne({_id: taskId})
+//         .exec()
+//         .then(result => {
+//             // res.status(202).json('Task was deleted');
+//             res.status(202).json(result);
+//         }).catch(err => {
+//         res.status(402).send('Task was not deleted');
+//     });
+// }
