122156dca6e40974149a3b6cd7694633af9e3679
Dmitrii126776 <dukek67@gmail.com>
2024-04-03 18:55:03 -0400

    getCardById.js

--- src/modules/cards/updateCard.js
+++ src/modules/cards/updateCard.js
@@ -3,15 +3,11 @@ import Card from './Model';
 export default function updateCard(req, res) {
     const cardId = req.params.cardId;
 
-    Card.findByIdAndUpdate(cardId, req.body, {new: true})
+    Card.findByIdAndUpdate(cardId, req.body)
         .exec()
         .then(result => {
-            if (!result) {
-                return res.status(404).send('Task not found')
-            }
-            res.status(200).json(result)
+            res.status(202).json('Card was updated')
         }).catch(err => {
-        console.error('Error updating task:', err);
-        res.status(500).send('Internal Server Error');
+        res.status(402).send('Card was not updated')
     });
 }
