501d8c03f72e937a9a59154ab5218c1405c509ab
Dmitrii126776 <dukek67@gmail.com>
2024-04-03 19:16:10 -0400

    getCardById.js

--- src/modules/cards/updateCard.js
+++ src/modules/cards/updateCard.js
@@ -3,6 +3,22 @@ import Card from './Model';
 export default function updateCard(req, res) {
     const cardId = req.params.cardId;
 
+    Card.findByIdAndUpdate(cardId, req.body, {new: true})
+        .exec()
+        .then(result => {
+            if (!result) {
+                return res.status(404).send('Card not found')
+            }
+            res.status(200).json(result)
+        }).catch(err => {
+        console.error('Error updating task:', err);
+        res.status(500).send('Internal Server Error');
+    });
+}
+/*
+export default function updateCard(req, res) {
+    const cardId = req.params.cardId;
+
     Card.findByIdAndUpdate(cardId, req.body)
         .exec()
         .then(result => {
@@ -11,3 +27,4 @@ export default function updateCard(req, res) {
         res.status(402).send('Card was not updated')
     });
 }
+*/
