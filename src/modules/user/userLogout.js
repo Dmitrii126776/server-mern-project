4785a2e94a7a94f73a156a301ff822574fa60335
Dmitrii126776 <dukek67@gmail.com>
2024-04-28 16:53:40 -0400

    userLogin.js

--- src/modules/user/userLogout.js
+++ src/modules/user/userLogout.js
@@ -1,4 +1,5 @@
 export default function userLogout(req, res) {
     // res.cookie('token', '').send();
-    res.cookie('token', '', { expires: new Date(0), httpOnly: true, secure: true, sameSite: 'strict' }).send();
+    // res.cookie('token', '', { expires: new Date(0), httpOnly: true, secure: true, sameSite: 'strict' }).send();
+    res.clearCookie('token').send();
 }
